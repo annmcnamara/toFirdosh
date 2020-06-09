d559a0c708d3c84b660e2b6fd6d336a6cf7f98f7
annmcnamara@gmail.com <annmcnamara@gmail.com>
2020-06-09 16:55:00 -0500

    MANY CHANGES - Map/Titles/Commas/Pie

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
 from covidData import CovidData
-from flask import Flask, jsonify, render_template
+from flask     import Flask, jsonify, render_template
 
-# set up database
+# Set up database
 data = CovidData("sqlite:///covidData.db")
 
-# set up flask
+# Set up flask
 app = Flask(__name__)
 
 # Flask Routes 
@@ -22,9 +22,13 @@ def show_apis():
         f'<a href="/api/v1.0/ids">/api/v1.0/states</a><br/>'       
         f'<a href="/api/v1.0/cases">/api/v1.0/cases</a><br/>' 
         f'<a href="/api/v1.0/cases/TX">/api/v1.0/cases/state_id</a><br/>' 
-        f'<a href="/api/v1.0/deaths">/api/v1.0/deaths/</a><br/>' 
+        f'<a href="/api/v1.0/deaths">/api/v1.0/deaths/2020-06-07</a><br/>' 
         f'<a href="/"><h4>Back</h4></a><br/>' 
     )    
+
+# The following routes return the data from our database in json format
+# They can be used to simply display the data on the web
+# We use it to populate our API
 
 @app.route("/api/v1.0/ids")
 def get_all_ids():
@@ -38,13 +42,10 @@ def get_all_user_results():
 def get_total_deaths(d):
     return jsonify(data.get_deaths(d)) 
 
-
 @app.route("/api/v1.0/cases/<state_id>")
 def get_one_user_results(state_id):
     print(state_id)
     return jsonify(data.get_cases(state_id))  
-
-
 
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
--- static/js/plots.js
+++ static/js/plots.js
@@ -1,128 +1,122 @@
-function buildMetadata(userData) {
 
-  result = userData;
-  //console.log("result build meta\n", result);
+var wholeMap = true;
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function buildMetadata(result) {
+
+  //NOTE 100 is LAST DATA POINT
+  //I WANT TO CHANGE THIS
+  state_name = result[98].State_Name;
+  cases      = result[98].Act_Total_Cases;
+  deaths     = result[98].Act_Total_Deaths;
+  population = result[98].Population;
+
+  buildPanels(deaths, cases, population);
+  buildPie(deaths, cases, population);
+
+}
+
+function buildPanels(deaths, cases, population){
+  //select and empty the panel div
   var PANEL = d3.select("#sample-metadata");
   PANEL.html("");
+  //add the data to the panel div
+  PANEL.append("h4").text(`${state_name}`);
+  PANEL.append("h6").text(`Cases: ${cases}`);
+  PANEL.append("h6").text(`Deaths: ${deaths}`);
+  PANEL.append("h6").text(`Population: ${population}`);
 
-  // Use `Object.entries` to add each key and value pair to the panel
-  // Hint: Inside the loop, you will need to use d3 to append new
-  // tags for each key-value in the metadata.  
-  // Object.entries(result).forEach(([key, value]) => {
-  //   PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
-  // }); 
-
-
-  function sum(obj) {
-    return Object.keys(obj).reduce((sum,key)=>sum+parseFloat(obj[key]||0),0);
-  }
- 
-  
-  PANEL.append("h5").text(`${result[0].State_Name}`)
-  //NOTE 93 is LAST DATA POINT
-  PANEL.append("h6").text(`Cases:${result[93].Act_Total_Cases}`)
-  PANEL.append("h6").text(`Deaths: ${result[93].Act_Total_Deaths}`)
-  PANEL.append("h6").text(`Population: ${result[93].Population}`)
-
-
-  cases      = result[93].Act_Total_Cases;
-  deaths     = result[93].Act_Total_Deaths;
-  population = result[93].Population;
+  //select and empty the cards 
+  //populate the cards
+  var DEATHS = d3.select("#numdeaths");
+  DEATHS.html("");
+  DEATHS.append("h2").text(`${(numberWithCommas(deaths))} `);
+  DEATHS.append("h3").text(`${((deaths/population)*100).toFixed(2)}%`);
 
   var CASES = d3.select("#numcases");
   CASES.html("");
-  CASES.append("h2").text(`${cases}`)
-  CASES.append("h3").text(`${((cases/population)*100).toFixed(2)}%`)
+  CASES.append("h2").text(`${(numberWithCommas(cases))}`);
+  CASES.append("h3").text(`${((cases/population)*100).toFixed(2)}%`);
 
-  var DEATHS = d3.select("#numdeaths");
-  DEATHS.html("");
-  DEATHS.append("h2").text(`${deaths} `)
-  DEATHS.append("h3").text(`${((deaths/population)*100).toFixed(2)}%`)
-
-
-  var unaffected = population - (cases + deaths)
+  var unaffected = population - (cases + deaths);
   var UNAFFECTED = d3.select("#numun");
   UNAFFECTED.html("");
-  UNAFFECTED.append("h2").text(`${unaffected} `)
-  UNAFFECTED.append("h3").text(`${((unaffected/population)*100).toFixed(2)}%`)
-
-
-
-
-  buildPie(result[93].Act_Total_Deaths, result[93].Act_Total_Cases, result[93].Population)
-
-  //buildGauge(result[0].data.Act_Total_Deaths * 1000000 / data.Population );
+  UNAFFECTED.append("h2").text(`${(numberWithCommas(unaffected))}`);
+  UNAFFECTED.append("h3").text(`${((unaffected/population)*100).toFixed(2)}%`);
 }
 
-function buildPie(deaths, cases, pop){
 
-  
+function buildPie(deaths, cases, pop){
   d = deaths/pop;
   c = cases/pop;
   u = pop - (deaths+cases)/pop;
 
-  console.log(d + " " + c + " " + u)
+  //console.log(d + " " + c + " " + u)
   unaffected = pop - (deaths+cases);
 
-  var pidata = [{
+  var piedata = [{
     values: [(deaths/pop), (cases/pop), (unaffected/pop)],
     labels: ['Deaths', 'Cases', 'Unaffected'],
-    //hole: 0.6,
+    hole: 0.6,
     text:"Cases",
     textposition: 'inside',
     name: 'Covid-19',
     hoverinfo: 'label+percent+name',
     type: 'pie', 
     marker: {
-      colors: ["red", "orange", "lightgray"], 
+      colors: ["#262626", "#6E6A6B", "#69AFFF"], 
       line: {width:.5}
 
     },
 
   }];
   
-  var pilayout = {
-    height: 300,
-    width: 400, 
-    //title_text:'Van Gogh: 5 Most Prominent Colors Shown Proportionally',
+  var pielayout = {
+    height: 290,
+    width: 300, 
     showlegend:true, 
-    margin:{l:25, r:20, t:10, b:20}, 
+    margin:{l:25, r:20, t:20, b:30}, 
     legend: {
-      x: 1,
+      x: 0,
       xanchor: 'left',
-      y: 0.3, 
-      orientation:'h'
-    }
+      y: -0.30, 
+      legend_orientation:'h'
+    }, 
   };
   
-  Plotly.newPlot('myPie', pidata, pilayout);
-
+  Plotly.newPlot('myPie', piedata, pielayout);
 }
 
+//GAUGE
+function createGauge(ranking){
+    console.log('GUAGE');
+    console.log('RANKING', ranking);
+     var element = document.querySelector('#guage-chart')
+     d3.select("#guage-chart").html("");
 
-
-function buildCharts(sample) {
+     // If ranking is 50 we want it to be 100 - the gauge goes from 0 to 100 but not greater
+     needle = ranking * 1.99;     
+     // The number to display is max 50 so we scale it down by 1/2 to get right number.              
+     midNo  = (Math.round(needle/2)).toString()
+     // Properties of the gauge
+     let gaugeOptions = {
+      hasNeedle: true,
+      needleColor: 'gray',
+      needleUpdateSpeed: 1000,
+      arcColors: ['#69AFFF', '#6E6A6B'],
+      arcDelimiters: [needle],
+      rangeLabel: ['0', '50'],
+      centralLabel: midNo,
+    }
+    
+     // Drawing and updating the chart
+     // element, chartwidth )(height is always 0.5 * chartWidth), options, value
+     // needle value is a number from 0 to 100 (not 100).
  
-  // var dataUrl = `http://127.0.0.1:5000/api/v1.0/cases/${sample}`;
-  var dataUrl = `/api/v1.0/cases/${sample}`;
-  //console.log(dataUrl)
-  var userInfo;
-  d3.json(dataUrl).then((data) => {
-    //console.log(data)
-
-    userInfo = data;
-
-    //console.log("POPULATION: ", userInfo)
-
-    var deaths = data.map(info => info.Act_Total_Deaths);
-    var cases  = data.map(info => info.Act_Total_Cases)
-    //console.log(deaths)
-    //console.log(deaths.length)
-   
-    buildMetadata(userInfo);
-
-  });
-
+     GaugeChart.gaugeChart(element, 300, gaugeOptions).updateNeedle(needle);
 }
 
 function makeBarChart(dates, trace1y, trace2y, statename, dataname, divname){
@@ -180,42 +174,37 @@ function makeBarChart(dates, trace1y, trace2y, statename, dataname, divname){
           },
           {step: 'all'}
         ]},
-      rangeslider: {range: ['2020-03-01', '2020-06-02']},
+      rangeslider: {range: ['2020-03-01', '2020-06-07']},
       type: 'date'
     },
+    yaxis: { 
+      title:"Number of " + dataname
+    }
   };
   
-  var update = {
-      opacity: 0.5,
-      marker: {
-      size: [40, 60, 80, 100],
-      color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)']
-    }
-  }
+  // var update = {
+  //     opacity: 0.5,
+  //     marker: {
+  //     size: [40, 60, 80, 100],
+  //     color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)']
+  //   }
+  // }
   
   Plotly.newPlot(divname, mydata, mylayout);
-
-
 }
 
-function makeBubble(state){
-  console.log("MAKE BUBBLE")
-  // var dataUrl = `http://127.0.0.1:5000/api/v1.0/cases/${state}`;
+function buildDashboard(state){
+  //Get the JSON data for the selected state
   var dataUrl = `/api/v1.0/cases/${state}`;
-  //console.log(dataUrl)
-  var userInfo;
+
   d3.json(dataUrl).then((data) => {
     //console.log(data)
-
-    // userInfo = data[0].Population;
-
-    // console.log("POPULATION: ", userInfo)
 
     var deaths     = data.map(d => d.Act_Total_Deaths);
     var deathspred = data.map(d => d.Est_Total_Deaths);
    
-    var cases     = data.map(d => d.Act_New_Cases);
-    var casespred = data.map(d => d.Est_New_Cases);
+    var cases      = data.map(d => d.Act_New_Cases);
+    var casespred  = data.map(d => d.Est_New_Cases);
 
     var dailydeaths     = data.map(d => d.Act_New_Deaths);
     var preddailydeaths = data.map(d => d.Estimated_New_Deaths);
@@ -223,246 +212,88 @@ function makeBubble(state){
 
     var dates  = data.map(info => info.Date);
     var states = data.map(info => info.State_Name);
-    var states_abbr = data.map(info => info.State_Abbr);
+    var states_abbr = data.map(info => info.State_Abbrev);
 
     var lat = data.map(d=>d.Latitude);
     var lon = data.map(d=>d.Longitude);
-    // console.log("LATITUDE: ", lat[0]);
-    // console.log("LONGITUDE: ", lon[0]);
-
-    // console.log(states_abbr)
-    // console.log(deaths)
-    // console.log(deaths.length)
 
     var ranking = data.map(d => d.Ranking_ATDPM)
     ranking = ranking[0];
-    console.log("RANKING: ", ranking)
 
+    //Build the Gauge
+    createGauge(ranking);
 
     // Build the charts
     // call function makeBarChart(dates, trace1y, trace2y, statename, dataname, divname){
 
     var statename = data[0].State_Name;
-    makeBarChart(dates, deaths,      deathspred, statename,      "Deaths",       "bubble");
-    makeBarChart(dates, cases,       casespred, statename,       "Daily Cases",  "bubble2");
+    makeBarChart(dates, deaths,      deathspred,      statename, "Deaths",       "bubble");
+    makeBarChart(dates, cases,       casespred,       statename, "Daily Cases",  "bubble2");
     makeBarChart(dates, dailydeaths, preddailydeaths, statename, "Daily Deaths", "dailydeaths");
 
-
-    var pidata = [{
-      values: [19, 26, 55],
-      labels: ['Deaths', 'Cases', 'Unaffected'],
-      hole: 0.4,
-      textposition: 'inside',
-      name: 'Covid-19',
-      hoverinfo: 'label+percent+name',
-      type: 'pie', 
-      marker: {
-        colors: ["black", "grey", "lightblue"]
-      },
-
-    }];
-    
-    var pilayout = {
-      height: 300,
-      width: 400
-    };
-    
-    //Plotly.newPlot('myPie', pidata, pilayout);
-
-    // var barData = [
-    //   {
-    //     x: dates,
-    //     y: deaths,
-    //     text:"",
-    //     type: "bar",
-    //     markder: {color:'lightblue'}
-    //     //orientation: "h",
-    //   }
-    // ];
-    // var barData2 = [
-    //   {
-    //     x: dates,
-    //     y: deathspred,
-    //     text:"",
-    //     type: "bar",
-    //     marker: {color:"green"}
-    //     //orientation: "h",
-    //   }
-    // ];
- 
-    // var barLayout = {
-    //   title: data[0].State_Name + ': Total Actual Deaths',
-    //   margin: { t: 30, l: 150 }
-    // };
-
-    // var barLayout2 = {
-    //   title: data[0].State_Name + ': Total Predicted Deaths',
-    //   margin: { t: 30, l: 150 },
-    //   color:deaths,
-    //   //yaxis: {range: [2,5]}
-    // };
- 
-    // Plotly.newPlot("alldeath", barData, barLayout);
-    // Plotly.newPlot("preddeath", barData2, barLayout2);
-
-
-
-    // var barData = [
-    //   {
-    //     x: dates,
-    //     y: cases,
-    //     text:"",
-    //     type: "bar",
-    //     markder: {color:'lightblue'}
-    //     //orientation: "h",
-    //   }
-    // ];
-    // var barData2 = [
-    //   {
-    //     x: dates,
-    //     y: casespred,
-    //     text:"",
-    //     type: "bar",
-    //     marker: {color:"green"}
-    //     //orientation: "h",
-    //   }
-    // ];
- 
-    // var barLayout = {
-    //   title: data[0].State_Name + ': Daily Actual Cases',
-    //   margin: { t: 30, l: 150 }
-    // };
-
-    // var barLayout2 = {
-    //   title: data[0].State_Name + ': Daily Predicted Cases',
-    //   margin: { t: 30, l: 150 },
-    //   color:deaths
-    // };
- 
-    // Plotly.newPlot("allcases", barData, barLayout);
-    // Plotly.newPlot("predcases", barData2, barLayout2);
-
-
-
-    // var barData = [
-    //   {
-    //     x: dates,
-    //     y: dailydeaths,
-    //     text:"",
-    //     type: "bar",
-    //     markder: {color:'lightblue'}
-    //     //orientation: "h",
-    //   }
-    // ];
-    // var barData2 = [
-    //   {
-    //     x: dates,
-    //     y: preddailydeaths,
-    //     text:"",
-    //     type: "bar",
-    //     marker: {color:"green"}
-    //     //orientation: "h",
-    //   }
-    // ];
- 
-    // var barLayout = {
-    //   title: data[0].State_Name + ': Daily Actual Deaths',
-    //   margin: { t: 30, l: 150 }
-    // };
-
-    // var barLayout2 = {
-    //   title: data[0].State_Name + ': Daily Predicted Deaths',
-    //   margin: { t: 30, l: 150 },
-    //   color:deaths
-    // };
- 
-    // Plotly.newPlot("dailydeaths", barData, barLayout);
-    // Plotly.newPlot("preddailydeaths", barData2, barLayout2);
-
-
-
-    //MAP
-    locations = [ "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY" ],
-
-    myURL = `/api/v1.0/deaths/2020-06-02`;
-
+    //Build the Map
+    myURL = `/api/v1.0/deaths/2020-06-07`;
     //console.log(dataUrl)
     d3.json(myURL).then((data) => {
-        //console.log(statename);
-        var deathsacc     = data.map(info => info.Act_Total_Deaths);
+        var deathsacc     = data.map(info => parseInt(info.Act_Total_Deaths));
+        var locations     = data.map(d=>d.State_Abbrev);
 
-        var mapdata = [{
-          type: "choroplethmapbox",
-          name: "US states",
-          geojson: "https://raw.githubusercontent.com/python-visualization/folium/master/examples/data/us-states.json",
-          locations: locations,
-         z:deathsacc,
-         zmin: 25, zmax: 280,  
-         colorscale:'Greys', opacity:.8,
-         colorbar: {y: 0, yanchor: "bottom", title: {text: "Deaths", side: "left"}}}
-       
-          ];
+        mapdata = [{
+          type: "choroplethmapbox", 
+          locations: locations, 
+          z: deathsacc,
+          geojson: "https://raw.githubusercontent.com/python-visualization/folium/master/examples/data/us-states.json", 
+          colorscale:'Blues', 
+          opacity:.8, 
+          reversescale:true,
+          colorbar: {
+            y: 0, 
+            yanchor: "bottom", 
+            side:"right", 
+            title: {
+              text: "Number of Actual Deaths", 
+              side:  "right"
+            }
+          }
+        }];
+
+        lat = lat[0]
+        lon = lon[0]
+
+        if(wholeMap == true){  //center on whole us
+          lon = -95.7129;
+          lat = 38.0902;
+          zoom = 7;
+          wholeMap = false;
+        }
+     
+        
+        var layout = {
+          mapbox: {
+            style:"light", 
+            center: {
+              lon: lon, 
+              lat: lat
+            }, 
+            zoom: 3, 
+            margin:{"r":0,"t":0,"l":0,"b":50}
+            },
+            width : 650, 
+            height: 600,
+            margin:{"r":0,"t":0,"l":0,"b":50}
+          };
+
+        var config = {
+          mapboxAccessToken: "pk.eyJ1IjoiYW5ubWNuYW1hcmEiLCJhIjoiY2s5YTNiOXI0MDNvOTNlbDdwOXdtejRiYSJ9.W1SBSUR6jrI3YgWdhDV2sA"
+        };
          
-         var layout = {mapbox: {style: "light", center: {lon: lon[0], lat: lat[0]}, zoom: 4},
-         width: 600, height: 400, margin: {t: 0, b: 100}};
-         
-         var config = {mapboxAccessToken: "pk.eyJ1IjoiYW5ubWNuYW1hcmEiLCJhIjoiY2s5YTNiOXI0MDNvOTNlbDdwOXdtejRiYSJ9.W1SBSUR6jrI3YgWdhDV2sA"};
-         
-         Plotly.newPlot('map', mapdata, layout, config);
+        Plotly.newPlot('map', mapdata, layout, config);
 
     });
  
 
-    var element = document.querySelector('#guage-chart')
-    d3.select("#guage-chart").html("");
-     /// NEED TO GET RANKING
-    //needle = Math.floor(Math.random() * 50);
-    needle = ranking * 1.999999999;  //if its 50 we want it to be 100 - the gauge goes from 0 to 100
-    ///console.log("NEEDLE", needle, "  ", needle/2);
-    midNo = (Math.round(needle/2)).toString() //the number to display is max 50 so we scale it down /2 to get right number.
-    // Properties of the gauge
-    let gaugeOptions = {
-     hasNeedle: true,
-     needleColor: 'gray',
-     needleUpdateSpeed: 1000,
-     arcColors: ['rgb(44, 151, 222)', 'lightgray'],
-     arcDelimiters: [needle],
-     rangeLabel: ['0', '50'],
-     centralLabel: midNo,
-   }
-   
-        // Drawing and updating the chart
-    // element, chartwidth )(height is always 0.5 * chartWidth), options, value
-    // needle value is a number from 0 to 100.
-
-    GaugeChart.gaugeChart(element, 300, gaugeOptions).updateNeedle(needle);
-
-    //  var element = document.querySelector('#guage-chart')
-    //  d3.select("#guage-chart").html("");
-    //   /// NEED TO GET RANKING
-     
-    //  total_deaths = data.map( v => v.Act_New_Deaths ).reduce( (total_deaths, current) => total_deaths + current, 0 );
-    //  statePopulaton = data[0].Population
-    //  needle = parseInt(total_deaths * 100 * 100 / (statePopulaton * 0.1545), 10);
-    //  console.log ("NEEDLE: ",needle)
-    //  midNo = needle.toString()
-    //  // Properties of the gauge
-    //  let gaugeOptions = {
-    //   hasNeedle: true,
-    //   needle: needle,
-    //   needleColor: 'gray',
-    //   needleUpdateSpeed: 1000,
-    //   arcColors: ['rgb(44, 151, 222)', 'lightgray'],
-    //   arcDelimiters: [needle],
-    //   rangeLabel: ['0', '100'],
-    //   centralLabel: midNo,
-    // }
-
-    //  // Drawing and updating the chart
-    //  // element, chartwidth )(height is always 0.5 * chartWidth), options, value
-    //  // needle value is a number from 0 to 100.
-    //  GaugeChart.gaugeChart(element, 300, gaugeOptions);
-
+    
+    //Build the Panels & Cards
     buildMetadata(data);
   });
 }
@@ -474,11 +305,11 @@ function init() {
   // Use the list of sample names to populate the select options
   d3.json("/api/v1.0/ids").then((data) => {
     var sampleNames = data;
-    console.log("States: ", sampleNames.length)
+    // console.log("States: ", sampleNames.length)
     // Use the first sample from the list to build the initial plots
     var firstSample = sampleNames[0];
     //console.log("Begin with:", firstSample)
-    makeBubble(firstSample)
+    buildDashboard(firstSample)
 
   });
 }
@@ -486,7 +317,7 @@ function init() {
 function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
   //console.log(`change ${{newSample}}`)
-  makeBubble(newSample);
+  buildDashboard(newSample);
   
 }
 
